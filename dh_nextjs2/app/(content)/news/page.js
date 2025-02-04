c8e09a3b122d71c9d02c5c11e506028d45233c04
psycn <psycn@itoplevel.com>
2025-02-04 19:01:32 +0900

    nextjs auth 학습완료

--- dh_nextjs2/app/(content)/news/page.js
+++ dh_nextjs2/app/(content)/news/page.js
@@ -1,17 +1,11 @@
+import { DUMMY_NEWS } from '@/dummy-news';
 import NewsList from '@/components/news-list';
 
-export default async function NewsPage() {
-  const response = await fetch('http://localhost:8080/news')
-
-  if(!response.ok){
-    throw new Error ('error')
-  }
-  const news = await response.json();
-  
+export default function NewsPage() {
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={news} />
+      <NewsList news={DUMMY_NEWS} />
     </>
   );
 }
